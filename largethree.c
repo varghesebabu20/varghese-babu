4ffb83db4a687e7d2979e704a935374040c0b827
varghese babu <varghesebabuk20@gmail.com>
2022-11-15 11:47:33 +0530

    My second program

---
+++ largethree.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter your number");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c)
+printf("%d is greater",a);
+else if(b>c)
+{
+printf("%d is greater",b);
+}
+else
+printf("%d is greater",c);
+}
